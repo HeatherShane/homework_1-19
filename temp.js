18ef3de238f4f9608333a311172ef669b9b8fa8e
HeatherShane <heather.j.shane@gmail.com>
2016-01-21 15:44:39 -0500

    Create temp.js

---
+++ temp.js
@@ -0,0 +1,13 @@
+var temp = 80
+var precipitation = false
+var indoors = true
+
+
+console.log("the temperature is " + temp + " degrees")
+
+if (temp > 80 && precipitation === false) {
+  console.log("time to swim");
+}
+else (indoors) {
+  console.log("time to swim")
+}
