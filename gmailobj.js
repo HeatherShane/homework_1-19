865c9a9c736893d442fafddce68f3eafdf8941ec
Jude Harding <jharding@genesisgroupusa.com>
2016-01-25 19:33:04 -0500

    Mailboxes, Circles, Deleted

---
+++ gmailobj.js
@@ -0,0 +1,21 @@
+var gmailobj = {
+
+  name: "Gmail",
+     mailboxes: [
+         'inbox',
+         'starred',
+         'sent',
+         'drafts'
+     ],
+     circles: [
+         'friends',
+         'family',
+         'aquantinsences',
+         'following',
+         'news'
+     ],
+
+     deleted: 'deleted',
+     sentmsgs: 'sent messages',
+
+};
